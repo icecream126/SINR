098276cf57ff303ea615b2d384c65ee7f0bd9e98
icecream126 <kim123hyomin@gmail.com>
2023-07-20 11:26:52 +0900

    remove other dataset related codes

--- commands_evaluation.sh
+++ commands_evaluation.sh
@@ -1,13 +1,3 @@
-# Basic INRs
-python eval_ginr.py lightning_logs/.../checkpoints/best.ckpt --dataset_dir dataset/bunny_v1       --n_fourier 100 --mesh data_generation/bunny/reconstruction/bun_zipper.ply --key bunny
-python eval_ginr.py lightning_logs/.../checkpoints/best.ckpt --dataset_dir dataset/protein_1AA7_A --n_fourier 100 --mesh data_generation/proteins/obj_files/1AA7_A.obj       --key protein_1AA7_A
-
-# Super-resolution
-python eval_super_resolution.py lightning_logs/.../checkpoints/best.ckpt --dataset_dir dataset/bunny_v1 --n_fourier 7
-
-# Reaction-diffusion
-python eval_reaction_diffusion.py lightning_logs/.../checkpoints/best.ckpt --dataset_dir dataset/bunny_time_full --n_fourier 100 --time=True --gif_sample_every 5
-
 # Weather modelling
 python eval_weather.py lightning_logs/.../checkpoints/best.ckpt --dataset_dir dataset/weather_time_gustsfc --n_fourier 100 --time=True --time_factor=2 --cmap Spectral --append gustsfc
 python eval_weather.py lightning_logs/.../checkpoints/best.ckpt --dataset_dir dataset/weather_time_dpt2m   --n_fourier 100 --time=True --time_factor=2 --cmap hot      --append dpt2m
