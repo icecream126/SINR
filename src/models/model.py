6ac44118c4a3dc198022aa2ad2e0116b6a113922
holywater2 <seongsukim@postech.ac.kr>
2023-09-20 11:37:38 +0900

    rmse fixed: using normalized mse

--- src/models/model.py
+++ src/models/model.py
@@ -1,4 +1,5 @@
 from typing import List, Union
+
 # from pytorch_lightning.utilities.types import EPOCH_OUTPUT
 import torch
 import pytorch_lightning as pl
@@ -49,21 +50,21 @@ class MODEL(pl.LightningModule):
             mse = mse.mean()
         else:
             mse = loss
+
+        rmse = torch.sqrt(mse)
         w_psnr_val = mse2psnr(mse.detach().cpu().numpy())
 
         self.log("batch_train_loss", loss, prog_bar=True, sync_dist=True)
         self.log("batch_train_mse", mse, prog_bar=True, sync_dist=True)
         self.log("batch_train_psnr", w_psnr_val, prog_bar=True, sync_dist=True)
-        self.log("batch_train_rmse",torch.sqrt(loss),prog_bar=True, sync_dist=True)
-        
-        return {"loss": loss, "batch_train_psnr":w_psnr_val.item()}
+        self.log("batch_train_rmse", rmse, prog_bar=True, sync_dist=True)
+
+        return {"loss": loss, "batch_train_psnr": w_psnr_val.item()}
 
     def train_epoch_end(self, outputs):
-        avg_train_mse = torch.stack(
-            [torch.tensor(x["loss"]) for x in outputs]
-        ).mean()
+        avg_train_mse = torch.stack([torch.tensor(x["loss"]) for x in outputs]).mean()
         avg_train_psnr = torch.stack(
-            [torch.tensor(x['batch_train_psnr']) for x in outputs]
+            [torch.tensor(x["batch_train_psnr"]) for x in outputs]
         ).mean()
         self.log("avg_train_mse", avg_train_mse)
         self.log("avg_train_psnr", avg_train_psnr, prog_bar=True, sync_dist=True)
@@ -86,7 +87,6 @@ class MODEL(pl.LightningModule):
             error = error.squeeze(-1)
         error = weights * error
         loss = error.mean()
-        rmse = torch.sqrt(loss)
 
         if self.normalize:
             self.scaler.match_device(pred)
@@ -100,6 +100,7 @@ class MODEL(pl.LightningModule):
         else:
             mse = loss
 
+        rmse = torch.sqrt(mse)
         w_psnr_val = mse2psnr(mse.detach().cpu().numpy())
 
         self.log("batch_test_loss", loss, prog_bar=True, sync_dist=True)
@@ -107,7 +108,11 @@ class MODEL(pl.LightningModule):
         self.log("batch_test_rmse", rmse)
         self.log("batch_test_mse", loss, prog_bar=True, sync_dist=True)
         self.log("batch_test_psnr", w_psnr_val)
-        return {"batch_test_mse": loss, "batch_test_psnr": w_psnr_val.item(), "batch_test_rmse": rmse.item()}
+        return {
+            "batch_test_mse": loss,
+            "batch_test_psnr": w_psnr_val.item(),
+            "batch_test_rmse": rmse.item(),
+        }
 
     def test_epoch_end(self, outputs):
         # Compute the average of test_mse and batch_test_psnr over the entire epoch
@@ -122,11 +127,14 @@ class MODEL(pl.LightningModule):
         ).mean()
 
         # Log the computed averages
-        self.log("avg_test_rmse",avg_test_rmse, prog_bar=True, sync_dist=True)
+        self.log("avg_test_rmse", avg_test_rmse, prog_bar=True, sync_dist=True)
         self.log("avg_test_mse", avg_test_mse, prog_bar=True, sync_dist=True)
         self.log("avg_test_psnr", avg_test_psnr, prog_bar=True, sync_dist=True)
         self.log(
-            "final_test_psnr", mse2psnr(avg_test_mse.detach().cpu().numpy()), prog_bar=True, sync_dist=True
+            "final_test_psnr",
+            mse2psnr(avg_test_mse.detach().cpu().numpy()),
+            prog_bar=True,
+            sync_dist=True,
         )
 
     def configure_optimizers(self):
@@ -136,7 +144,11 @@ class MODEL(pl.LightningModule):
             optimizer, factor=0.5, patience=self.lr_patience, verbose=True
         )
 
-        sch_dict = {"scheduler": scheduler, "monitor": "batch_train_mse", "frequency": 1}
+        sch_dict = {
+            "scheduler": scheduler,
+            "monitor": "batch_train_mse",
+            "frequency": 1,
+        }
         return {"optimizer": optimizer, "lr_scheduler": sch_dict}
 
 
@@ -156,15 +168,17 @@ class DENOISING_MODEL(pl.LightningModule):
         )  # [512, 3] for each
         mean_lat_weight = data["mean_lat_weight"]  # 512
 
-        weights = torch.abs(torch.cos(inputs[..., 0]))  # [512, 1] # make sure nonzero weights
+        weights = torch.abs(
+            torch.cos(inputs[..., 0])
+        )  # [512, 1] # make sure nonzero weights
         weights = weights / mean_lat_weight  # [512, 512]
 
         inputs = to_cartesian(inputs)
         pred = self.forward(inputs)
 
-        error = torch.sum((pred - target) ** 2, dim=-1)# , keepdim=True)
-        error_orig = torch.sum((pred - g_target) ** 2, dim=-1)# , keepdim=True)
-        if len(error.shape)>len(weights.shape):
+        error = torch.sum((pred - target) ** 2, dim=-1)  # , keepdim=True)
+        error_orig = torch.sum((pred - g_target) ** 2, dim=-1)  # , keepdim=True)
+        if len(error.shape) > len(weights.shape):
             error = error.squeeze(-1)
         error = weights * error
         error_orig = weights * error_orig
@@ -190,21 +204,26 @@ class DENOISING_MODEL(pl.LightningModule):
         ).mean()
         # self.log("avg_train_mse", avg_train_mse)
         self.log("avg_train_mse_orig", avg_train_mse_orig)
-        self.log("final_train_psnr_orig", mse2psnr(avg_train_mse_orig.detach().cpu().numpy()), prog_bar=True, sync_dist=True)
+        self.log(
+            "final_train_psnr_orig",
+            mse2psnr(avg_train_mse_orig.detach().cpu().numpy()),
+            prog_bar=True,
+            sync_dist=True,
+        )
 
     def test_step(self, data, batch_idx):
         inputs, target, g_target = data["inputs"], data["target"], data["g_target"]
         mean_lat_weight = data["mean_lat_weight"]
 
-        weights = torch.abs(torch.cos(inputs[..., 0])) # make sure nonnegative weights
+        weights = torch.abs(torch.cos(inputs[..., 0]))  # make sure nonnegative weights
         weights = weights / mean_lat_weight
 
         inputs = to_cartesian(inputs)
         pred = self.forward(inputs)
 
-        error = torch.sum((pred - target) ** 2, dim=-1)# , keepdim=True)
-        error_orig = torch.sum((pred - g_target) ** 2, dim=-1)# , keepdim=True)
-        if len(error.shape)>len(weights.shape):
+        error = torch.sum((pred - target) ** 2, dim=-1)  # , keepdim=True)
+        error_orig = torch.sum((pred - g_target) ** 2, dim=-1)  # , keepdim=True)
+        if len(error.shape) > len(weights.shape):
             error = error.squeeze(-1)
         error = weights * error
         error_orig = weights * error_orig
@@ -244,9 +263,24 @@ class DENOISING_MODEL(pl.LightningModule):
         self.log("avg_test_mse", avg_test_mse, prog_bar=True, sync_dist=True)
         self.log("avg_test_mse_orig", avg_test_mse_orig, prog_bar=True, sync_dist=True)
         self.log("avg_test_psnr", avg_batch_test_psnr, prog_bar=True, sync_dist=True)
-        self.log("avg_test_psnr_orig", avg_batch_test_psnr_orig, prog_bar=True, sync_dist=True)
-        self.log("final_test_psnr", mse2psnr(avg_test_mse.detach().cpu().numpy()), prog_bar=True, sync_dist=True)
-        self.log("final_test_psnr_orig", mse2psnr(avg_test_mse_orig.detach().cpu().numpy()), prog_bar=True, sync_dist=True)
+        self.log(
+            "avg_test_psnr_orig",
+            avg_batch_test_psnr_orig,
+            prog_bar=True,
+            sync_dist=True,
+        )
+        self.log(
+            "final_test_psnr",
+            mse2psnr(avg_test_mse.detach().cpu().numpy()),
+            prog_bar=True,
+            sync_dist=True,
+        )
+        self.log(
+            "final_test_psnr_orig",
+            mse2psnr(avg_test_mse_orig.detach().cpu().numpy()),
+            prog_bar=True,
+            sync_dist=True,
+        )
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
