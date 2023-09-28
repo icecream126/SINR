6a26d140a93f7d32334b7b1da016cc22a4343e0b
icecream126 <kim123hyomin@gmail.com>
2023-09-29 03:20:02 +0900

    update sh gauss_act sweep

--- src/models/swinr.py
+++ src/models/swinr.py
@@ -13,8 +13,8 @@ class SphericalGaborLayer(nn.Module):
         time,
         omega,
         sigma,
-        learn_rotate,
-        learn_dilate,
+        stop_rotate,
+        stop_dilate,
         **kwargs,
     ):
         super().__init__()
@@ -24,12 +24,15 @@ class SphericalGaborLayer(nn.Module):
         self.sigma = sigma
         self.output_dim = output_dim
         
-        self.dilate = nn.Parameter(torch.empty(1, output_dim), requires_grad=learn_rotate)
+        print('stop_rotate : ',stop_rotate)
+        print('stop_dilate : ',stop_dilate)
+        
+        self.dilate = nn.Parameter(torch.empty(1, output_dim), requires_grad=stop_rotate)
         nn.init.normal_(self.dilate)
 
-        self.u = nn.Parameter(torch.empty(output_dim), requires_grad=learn_dilate)
-        self.v = nn.Parameter(torch.empty(output_dim), requires_grad=learn_dilate)
-        self.w = nn.Parameter(torch.empty(output_dim), requires_grad=learn_dilate)
+        self.u = nn.Parameter(torch.empty(output_dim), requires_grad=stop_dilate)
+        self.v = nn.Parameter(torch.empty(output_dim), requires_grad=stop_dilate)
+        self.w = nn.Parameter(torch.empty(output_dim), requires_grad=stop_dilate)
         nn.init.uniform_(self.u)
         nn.init.uniform_(self.v)
         nn.init.uniform_(self.w)
@@ -117,6 +120,8 @@ class INR(MODEL):
         skip,
         omega,
         sigma,
+        stop_rotate,
+        stop_dilate,
         **kwargs,
     ):
         super().__init__(**kwargs)
@@ -128,52 +133,7 @@ class INR(MODEL):
         self.first_nonlin = SphericalGaborLayer
 
         self.net = nn.ModuleList()
-        self.net.append(self.first_nonlin(hidden_dim, time, omega, sigma))
-
-        self.nonlin = ReLULayer
-
-        for i in range(hidden_layers):
-            if skip and i == ceil(hidden_layers / 2):
-                self.net.append(self.nonlin(hidden_dim + input_dim, hidden_dim))
-            else:
-                self.net.append(self.nonlin(hidden_dim, hidden_dim))
-
-        final_linear = nn.Linear(hidden_dim, output_dim)
-
-        self.net.append(final_linear)
-
-    def forward(self, x):
-        x_in = x
-        for i, layer in enumerate(self.net):
-            if self.skip and i == ceil(self.hidden_layers / 2) + 1:
-                x = torch.cat([x, x_in], dim=-1)
-            x = layer(x)
-        return x
-
-
-class DENOISING_INR(DENOISING_MODEL):
-    def __init__(
-        self,
-        input_dim,
-        output_dim,
-        hidden_dim,
-        hidden_layers,
-        time,
-        skip,
-        omega,
-        sigma,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-
-        self.time = time
-        self.skip = skip
-        self.hidden_layers = hidden_layers
-
-        self.first_nonlin = SphericalGaborLayer
-
-        self.net = nn.ModuleList()
-        self.net.append(self.first_nonlin(hidden_dim, time, omega, sigma))
+        self.net.append(self.first_nonlin(hidden_dim, time, omega, sigma, stop_rotate, stop_dilate))
 
         self.nonlin = ReLULayer
 
